91c609a2e556f7d0f969f13fbfbe3b8508770143
Koray Guel <koray1@uni-bremen.de>
2024-06-16 22:41:05 +0200

    Initial commit

---
+++ src/main/java/org/example/GUI.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+        Scene scene = new Scene(root, 300, 200);
+        stage.setTitle("JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
