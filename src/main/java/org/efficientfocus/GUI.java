a4794b38155fcd8c41543ca617fcc322249524b9
Koray Guel <koray1@uni-bremen.de>
2024-06-16 23:30:34 +0200

    fifth update

--- src/main/java/org/efficientfocus/GUI.java
+++ src/main/java/org/efficientfocus/GUI.java
@@ -2,13 +2,19 @@ package org.efficientfocus;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 public class GUI extends Application {
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Age Verification");
+        Label ageLabel = new Label("Please enter your age:");
+        TextField ageField = new TextField();
         Label label = new Label("Hello, JavaFX!");
         StackPane root = new StackPane();
         root.getChildren().add(label);
