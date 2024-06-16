f98108746ae28ac2901a45276c1462417f7591f8
Koray Guel <koray1@uni-bremen.de>
2024-06-16 23:33:34 +0200

    starting the age permission

--- src/main/java/org/efficientfocus/GUI.java
+++ src/main/java/org/efficientfocus/GUI.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class GUI extends Application {
